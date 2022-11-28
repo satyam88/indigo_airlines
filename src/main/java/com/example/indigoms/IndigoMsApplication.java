beff1d498125d63d101e2106410f6b6d41a035fc
Satyam Pandey <satyam@gmail.com>
2022-11-28 08:13:08 +0530

    hello

---
+++ src/main/java/com/example/indigoms/IndigoMsApplication.java
@@ -0,0 +1,13 @@
+package com.example.indigoms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IndigoMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IndigoMsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/indigoms/IndigoMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.indigoms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndigoMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
