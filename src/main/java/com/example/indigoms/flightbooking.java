5336add307978d39bd7ceb90cd3cbf1d12a64ef2
Satyam Pandey <satyam@gmail.com>
2022-11-28 08:49:14 +0530

    codepush

---
+++ src/main/java/com/example/indigoms/flightbooking.java
@@ -0,0 +1,13 @@
+package com.example.indigoms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class flightbooking {
+    @GetMapping("/myflightbooking")
+    public String getData(){
+        return "Please book your flight ticket";
+    }
+}
