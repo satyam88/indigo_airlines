8955a158d93a1a2e7b02633fe79984490a415cdb
Satyam Pandey <satyam@gmail.com>
2022-11-28 08:56:53 +0530

    codeush

---
+++ src/main/java/com/example/indigoms/webcheckin.java
@@ -0,0 +1,13 @@
+package com.example.indigoms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class webcheckin {
+    @GetMapping("/mywebcheckin")
+    public String getData(){
+        return "Please do web checkin before 24 hours of flight";
+    }
+}
